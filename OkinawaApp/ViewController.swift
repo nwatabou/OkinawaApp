f1cf6599d0b422340309e3f9ed80d2c9c5f89c6f
nwatabou <wataru1.11@gmail.com>
2019-07-07 13:43:42 +0900

    GoogleMapを表示できるところまで実装

--- OkinawaApp/ViewController.swift
+++ OkinawaApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: 26.2140964, longitude: 127.6824531, zoom: 10.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+    }
 }
 
