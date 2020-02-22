409faae26cc4b7586d77ec33a9b6e04ffc12f84e
nwatabou <wataru1.11@gmail.com>
2020-02-22 20:55:12 +0900

    チェックボックスのタップを実装

--- OkinawaApp/View/IslandTableViewCell.swift
+++ OkinawaApp/View/IslandTableViewCell.swift
@@ -18,13 +18,43 @@ class IslandTableViewCell: UITableViewCell {
     @IBOutlet weak var checkButton: UIButton!
     @IBOutlet weak var titileLabel: UILabel!
 
-    private let disposeBag = DisposeBag()
+    private var disposeBag = DisposeBag()
+    private var isChecked: Bool = false {
+        didSet {
+            if isChecked {
+                checkButton.setImage(checkedImage, for: .normal)
+            } else {
+                checkButton.setImage(uncheckedImage, for: .normal)
+            }
+        }
+    }
+    private let checkedImage = UIImage(named: "check_box")
+    private let uncheckedImage = UIImage(named: "check_box_outline")
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        commonInit()
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+        isChecked = false
+        titileLabel.text = nil
     }
 
     func setData(_ island: Island) {
         titileLabel.text = island.name
+    }
+
+    func tapped() {
+        isChecked = !isChecked
+    }
+
+    private func commonInit() {
+        checkButton.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                self?.tapped()
+            }).disposed(by: disposeBag)
     }
 }
--- OkinawaApp/ViewController/IslandsListViewController.swift
+++ OkinawaApp/ViewController/IslandsListViewController.swift
@@ -55,4 +55,8 @@ extension IslandsListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return IslandTableViewCell.cellHeight
     }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
