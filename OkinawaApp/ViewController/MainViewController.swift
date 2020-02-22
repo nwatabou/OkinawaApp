84a3cec91dfbdc231f702351e42de2769bd5a39f
nwatabou <wataru1.11@gmail.com>
2020-02-22 17:20:04 +0900

    ピンが全て表示されるよう修正

--- OkinawaApp/ViewController/MainViewController.swift
+++ OkinawaApp/ViewController/MainViewController.swift
@@ -15,24 +15,29 @@ class MainViewController: UIViewController {
 
     private let viewModel = MapViewModel()
     private let disposeBag = DisposeBag()
+    private let defaultPosition = CLLocationCoordinate2D(latitude: 26.2140964, longitude: 127.6824531)
 
     private var map: GMSMapView!
-    private var islandList = [Island]() {
-        didSet {
-            reloadMap()
-        }
-    }
+    private var islandList = [Island]()
     private var markers = [GMSMarker]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadView()
         configView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        guard !islandList.isEmpty else { return }
+        reloadMap()
     }
 
     override func loadView() {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: 26.2140964, longitude: 127.6824531, zoom: 10.0)
+        let camera = GMSCameraPosition.camera(withTarget: defaultPosition, zoom: 12.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
         map = mapView
@@ -49,12 +54,16 @@ class MainViewController: UIViewController {
     }
 
     private func reloadMap() {
+        var bounds = GMSCoordinateBounds()
         for island in islandList {
             let location = toLocation(latitude: island.latitude, longitude: island.longitude)
+            bounds = bounds.includingCoordinate(location)
             let marker = GMSMarker(position: location)
             marker.title = island.name
             marker.map = map
         }
+        let cameraUpdate = GMSCameraUpdate.fit(bounds, withPadding: 16.0)
+        map.moveCamera(cameraUpdate)
     }
 
     private func toLocation(latitude: Float, longitude: Float) -> CLLocationCoordinate2D {
